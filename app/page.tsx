2533a27aa84fb814908674ec0b98934020377a94
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2023-01-19 13:31:14 +0500

    assignment

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+<h1 className="bg-red-500 text-gray-50 text-center font-extrabold">Some Tailwind css Fonts</h1>
+
+      <h1 className="font-sans">
+        This is font-san
+      </h1>
+      <h1 className="font-serif">
+      This is font-serif
+      </h1>
+      <h1 className="font-mono">
+      This is font-mono
+      </h1>
+      <h1 className="font-extralight">
+      This is font-extralight
+      </h1>
+    </>
+  );
+}
